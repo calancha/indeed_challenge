b00e248dd80577ac97b564c7461f9777f6f2e43a
Tino Calancha <tino.calancha@gmail.com>
2018-09-02 18:08:22 +0900

    Improved version
    * paint.cc: Simplify code and correct algorithm

--- paint.cc
+++ paint.cc
@@ -37,49 +37,53 @@ struct Dot {
 
 typedef struct Dot st_dot;
 
-// Predicate: return true if dot is a neighbour for the cell at (row, col)
-bool is_a_neighbour(int row, int col, const st_dot &dot)
-{
-  if (row == (dot.row - 1) && col == dot.col) return true; // up
-  if (row == (dot.row + 1) && col == dot.col) return true; // down
-  if (row == dot.row && col == (dot.col - 1)) return true; // left
-  if (row == dot.row && col == (dot.col + 1)) return true; // right
-  return false;
-}
-
-// Check if cell(row, col) has any dot neighbour
-// Worse case: O(n_duts)
-bool has_dot_neighbour(int row, int col, vector<st_dot> &dots)
-{
-  return any_of(dots.begin(),
-                dots.end(),
-                [&row, &col](const st_dot &dot) { return is_a_neighbour(row, col, dot); });
-}
-
-
-
-// Return referece to dot neighbour with maximum darkness
-st_dot &max_dot_neighbour(int row, int col, vector<st_dot> &dots)
-{
-  return dots.front(); // TODO IMPLEMENT IT  
-}
-
+
 // Predicate: return true if cell at (row, col) is a dot.
-bool is_a_dot(int row, int col, vector<st_dot> &dots)
+// Complexity: Worse case O(n_dots)
+bool is_a_dot(int row, int col, const vector<st_dot> &dots)
 {
   return any_of(dots.begin(),
                 dots.end(),
-                [&row, &col](const st_dot &dot) { return row == dot.row && col == dot.col; });
+                [&row, &col](const st_dot &dot)
+                { return row == dot.row && col == dot.col; });
 }
 
 // Return the dot at cell (row, col)
-st_dot &dot_at(int row, int col, vector<st_dot> &dots)
+// Complexity: Worse case O(n_dots)
+const st_dot &dot_at(int row, int col, const vector<st_dot> &dots)
 {
   for (auto &dot : dots)
     if (row == dot.row && col == dot.col)
       return dot;
 }
 
+
+// Return minimum path from DOT to the cell at (row, col)
+// I assume the minimum distance is: just adding the
+// difference on cols/rows.
+// Complexity: Constant time.
+int min_path(int row, int col, const st_dot &dot)
+{
+  return abs(dot.row - row) + abs(dot.col - col);
+}
+
+// Maximum ink value: check all dots
+// Complexity: O(n_dots).
+int max_ink_value(int row, int col, const vector<st_dot> &dots)
+{
+  if (is_a_dot(row, col, dots)) return dot_at(row, col, dots).darkness; // O(n_dots)
+  vector<int> ink_values;
+  // Complexity: O(n_dots)
+  for (const auto &dot : dots) {
+    int value = dot.darkness - min_path(row, col, dot);
+    if (value < 0) value = 0;
+    ink_values.push_back(value);
+  }
+  return *min_element(ink_values.begin(), ink_values.end()); // O(n_dots)
+}
+
+
+
 int main() {
   /* Enter your code here. Read input from STDIN. Print output to STDOUT */
 #ifdef DEBUG 
@@ -103,14 +107,12 @@ int main() {
       dots.push_back(dot);
     }
     int result = 0;
+    // Complexity: O( height*width ) * O( n_dots )
+    // First part from the 2 loops; the second is the complexity of
+    // `max_ink_value'.
     for (int row=0;row<height;++row) {
       for (int col=0;col<width;++col) {
-        if (is_a_dot(row, col, dots)) {
-          result += dot_at(row, col, dots).darkness;
-        } else if (has_dot_neighbour(row, col, dots)) {
-          st_dot max_dot = max_dot_neighbour(row, col, dots);
-          result += (max_dot.darkness - 1);
-        }
+        result += max_ink_value(row, col, dots);
       }
     }
     cout << result << endl;
