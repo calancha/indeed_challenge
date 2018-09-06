51589feab3c9e35825b020dc85eaea178339b13f
Tino Calancha <tino.calancha@gmail.com>
2018-09-06 17:49:39 +0900

    * paint.cc: Include just libraries actually used

--- paint.cc
+++ paint.cc
@@ -1,26 +1,7 @@
 #include <fstream>
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
 #include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
 #include <algorithm>
-#include <unordered_map>
 
 /*
   Comment the following line when running in HackerRank.
